1bc86fa70b15a93ddc220049ccdacbe3b17f2bc8
amedora <americandragsterracing@gmail.com>
2024-10-28 11:03:15 +0900

    new deploy.yml

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/downhillsukisuki/',
   title: "ダウンヒル☆スキスキ",
   description: "ダウンヒルスケートボードの雑多なメモ",
   themeConfig: {
